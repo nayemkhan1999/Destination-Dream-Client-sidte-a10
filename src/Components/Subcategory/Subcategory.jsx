1c7e0d1ec9da080f352cf8973eeb823bb020ca96
nayemkhan1999 <1999workmail@gmail.com>
2024-04-30 16:25:45 +0600

    updated firebase config

--- src/Components/Subcategory/Subcategory.jsx
+++ src/Components/Subcategory/Subcategory.jsx
@@ -1,12 +1,26 @@
 import { useLoaderData } from "react-router-dom";
 import SubcategoryCard from "../SubcategoryCard/SubcategoryCard";
+import { Typewriter } from "react-simple-typewriter";
 
 const Subcategory = () => {
   const categoryLoader = useLoaderData();
   console.log(categoryLoader);
   return (
     <div>
-      <h2>sub category</h2>
+      <h2 className="lg:text-5xl text-3xl tracking-widest opacity-75 font-bold mt-10">
+        sub
+        <span className="text-[#F97316]">
+          <Typewriter
+            cursor
+            cursorBlinking
+            delaySpeed={1000}
+            deleteSpeed={25}
+            loop={0}
+            typeSpeed={75}
+            words={["category Card"]}
+          />
+        </span>
+      </h2>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
         {categoryLoader.map((catCard) => (
           <SubcategoryCard
--- src/Firebase.config.js
+++ src/Firebase.config.js
@@ -1,14 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD54NikR-LF3_1_otHXW606mxbDifrU-uw",
-  authDomain: "destination-dream-ebfea.firebaseapp.com",
-  projectId: "destination-dream-ebfea",
-  storageBucket: "destination-dream-ebfea.appspot.com",
-  messagingSenderId: "500630196058",
-  appId: "1:500630196058:web:6f6551135c1ec1b1e42d63",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
